a063e1b0dce4a8a98d005a802a76361fe5974cee
KryukovAndrey <kryukov.ios@yandex.ru>
2020-05-20 23:24:06 +0300

    TrafficLights is working !)

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -10,11 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var yellowView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    @IBOutlet weak var textChangeBottonColor: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = redView.frame.size.height / 2
+        yellowView.layer.cornerRadius = yellowView.frame.size.height / 2
+        greenView.layer.cornerRadius = greenView.frame.size.height / 2
+        textChangeBottonColor.layer.cornerRadius = textChangeBottonColor.frame.size.height / 4
     }
 
-
+    @IBAction func ChangeBottonColor() {
+        
+        if redView.alpha != 1 && yellowView.alpha != 1 && greenView.alpha != 1 {
+            redView.alpha = 1
+            textChangeBottonColor.setTitle("Next", for: .normal)
+        } else
+        
+        if redView.alpha == 1 {
+            redView.alpha = 0.3
+            yellowView.alpha = 1
+        } else
+        
+        if yellowView.alpha == 1 {
+            yellowView.alpha = 0.3
+            greenView.alpha = 1
+        } else
+        
+        if greenView.alpha == 1 {
+            greenView.alpha = 0.3
+            redView.alpha = 1
+        }
+    }
 }
 
